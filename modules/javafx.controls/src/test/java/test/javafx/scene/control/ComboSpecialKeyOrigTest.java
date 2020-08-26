6c13f27c0400c42328193e58427362f9b250ba94
kleopatra <fastegal@swingempire.de>
2020-08-26 16:06:31 +0200

    merged test from local test collections, 2

--- modules/javafx.controls/src/test/java/test/javafx/scene/control/ComboSpecialKeyOrigTest.java
+++ modules/javafx.controls/src/test/java/test/javafx/scene/control/ComboSpecialKeyOrigTest.java
@@ -39,23 +39,17 @@ import org.junit.runners.Parameterized;
 
 import com.sun.javafx.tk.Toolkit;
 
-import static java.util.stream.Collectors.*;
 import static javafx.scene.input.KeyCode.*;
 import static javafx.scene.input.KeyEvent.*;
 import static org.junit.Assert.*;
 
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
 import javafx.scene.control.skin.ComboBoxPopupControl;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -64,20 +58,13 @@ import test.com.sun.javafx.pgstub.StubToolkit;
 import test.com.sun.javafx.scene.control.infrastructure.KeyEventFirer;
 
 /**
- * Tests for issues around handling of various special keys.
- * <p>
- * part of https://bugs.openjdk.java.net/browse/JDK-8234247 - ESCAPE
- * - guarantee correct behavior of editors with textFormatter
- * - guarantee correct event dispatch sequence for handlers
- *
- * <p>
- * https://bugs.openjdk.java.net/browse/JDK-8233040 - F4
+ * Test for https://bugs.openjdk.java.net/browse/JDK-8233040 - F4
  * must not be consumed by EventFilter in ComboBoxPopupControl.
  * <p>
  * Parameterized in concrete sub of ComboBoxBase and editable.
  */
 @RunWith(Parameterized.class)
-public class ComboSpecialKeyTest {
+public class ComboSpecialKeyOrigTest {
 
     private Toolkit tk;
     private Scene scene;
@@ -88,103 +75,10 @@ public class ComboSpecialKeyTest {
     private Supplier<ComboBoxBase> comboFactory;
     private boolean editable;
 
-//------------ esc
-
-    /**
-     * test to guarantee correct behavior for editors with textformatter
-     */
-    @Test
-    public void testEscapeEditorWithTextFormatter() {
-        if (!editable) return;
-        showAndFocus();
-        TextFormatter<String> formatter = new TextFormatter<>(TextFormatter.IDENTITY_STRING_CONVERTER);
-        TextField editor = null;
-        if (comboBox instanceof ComboBox) {
-            editor = ((ComboBox) comboBox).getEditor();
-        } else if (comboBox instanceof DatePicker) {
-            editor = ((DatePicker) comboBox).getEditor();
-        }
-        if (editor == null) {
-            fail("unexpected state: " + editable + " " + comboBox.getClass().getName());
-        }
-        editor.setTextFormatter(formatter);
-        KeyEventFirer firer = new KeyEventFirer(null, scene);
-        firer.doKeyTyped(A);
-        assertEquals("A", editor.getText());
-        firer.doKeyPress(ESCAPE);
-        assertEquals("", editor.getText());
-    }
-
-    @Test
-    public void testEventSequenceEscapePressedHandler() {
-        showAndFocus();
-        List<Event> events = new ArrayList<>();
-        EventHandler<KeyEvent> adder = events::add;
-        scene.addEventHandler(KEY_PRESSED, adder);
-        root.addEventHandler(KEY_PRESSED, adder);
-        comboBox.addEventHandler(KEY_PRESSED, adder);
-        KeyCode key = ESCAPE;
-        KeyEventFirer keyFirer = new KeyEventFirer(comboBox);
-        keyFirer.doKeyPress(key);
-        assertEquals(failPrefix() + " event count", 3, events.size());
-        List<Object> sources = events.stream()
-                .map(e -> e.getSource())
-                .collect(toList());
-        List<Object> expected = List.of(comboBox, root, scene);
-        assertEquals(failPrefix(), expected, sources);
-    }
-
-    @Test
-    public void testEventSequenceEscapeReleasedHandler() {
-        showAndFocus();
-        List<Event> events = new ArrayList<>();
-        EventHandler<KeyEvent> adder = events::add;
-        scene.addEventHandler(KEY_RELEASED, adder);
-        root.addEventHandler(KEY_RELEASED, adder);
-        comboBox.addEventHandler(KEY_RELEASED, adder);
-        KeyCode key = ESCAPE;
-        KeyEventFirer keyFirer = new KeyEventFirer(comboBox);
-        keyFirer.doKeyPress(key);
-        assertEquals(failPrefix() + " event count", 3, events.size());
-        List<Object> sources = events.stream()
-                .map(e -> e.getSource())
-                .collect(toList());
-        List<Object> expected = List.of(comboBox, root, scene);
-        assertEquals(failPrefix(), expected, sources);
-    }
-
-//---------- enter
-
-    /**
-     * Test related to https://bugs.openjdk.java.net/browse/JDK-8207759
-     * broken event dispatch sequence by forwardToParent.
-     *
-     * passes after fixing the textfield bug
-     */
-    @Test
-    public void testEventSequenceEnterPressedHandler() {
-        showAndFocus();
-        List<Event> events = new ArrayList<>();
-        EventHandler<KeyEvent> adder = events::add;
-        scene.addEventHandler(KEY_PRESSED, adder);
-        root.addEventHandler(KEY_PRESSED, adder);
-        comboBox.addEventHandler(KEY_PRESSED, adder);
-        KeyCode key = ENTER;
-        KeyEventFirer keyFirer = new KeyEventFirer(null, scene);
-        keyFirer.doKeyPress(key);
-        assertEquals("event count", 3, events.size());
-        List<Object> sources = events.stream()
-                .map(e -> e.getSource())
-                .collect(toList());
-        List<Object> expected = List.of(comboBox, root, scene);
-        assertEquals(expected, sources);
-    }
-
-//------------------ f4
-
     @Test
     public void testF4TogglePopup() {
         showAndFocus();
+        comboBox.setEditable(editable);
         assertFalse(comboBox.isShowing());
         KeyEventFirer firer = new KeyEventFirer(comboBox);
         firer.doKeyPress(F4);
@@ -196,6 +90,7 @@ public class ComboSpecialKeyTest {
     @Test
     public void testF4ConsumeFilterNotTogglePopup() {
         showAndFocus();
+        comboBox.setEditable(editable);
         List<KeyEvent> events = new ArrayList<>();
         comboBox.addEventFilter(KEY_RELEASED, e -> {
             if (e.getCode() == F4) {
@@ -229,7 +124,7 @@ public class ComboSpecialKeyTest {
         return Arrays.asList(data);
     }
 
-    public ComboSpecialKeyTest(Supplier<ComboBoxBase> factory, boolean editable) {
+    public ComboSpecialKeyOrigTest(Supplier<ComboBoxBase> factory, boolean editable) {
         this.comboFactory = factory;
         this.editable = editable;
     }
@@ -242,7 +137,6 @@ public class ComboSpecialKeyTest {
         showAndFocus();
         List<Node> expected = List.of(comboBox);
         assertEquals(expected, root.getChildren());
-        assertEquals(editable, comboBox.isEditable());
     }
 
      protected void showAndFocus() {
@@ -273,8 +167,6 @@ public class ComboSpecialKeyTest {
         stage = new Stage();
         stage.setScene(scene);
         comboBox = comboFactory.get();
-        // Note: safer to do here (vs. in the test methods) to not forget doing it
-        comboBox.setEditable(editable);
         root.getChildren().addAll(comboBox);
     }
 
